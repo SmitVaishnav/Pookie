7724960a2c59e823bcd1f8c3ea9d5f446882eb0b
smitvaishnav21 <smitvaishnav007@gmail.com>
2024-05-19 00:23:20 +0530

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,18 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
       "What if I asked really nicely?",
       "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
+      "With a Waffle?",
+      "What about a matcha frostie?",
       "PLEASE POOKIE",
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking to my ghost",
       "please babe",
       ":((((",
       "PRETTY PLEASE",
-      "Estoy muerto",
       "No :(",
     ];
 
@@ -37,7 +35,7 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="https://media1.tenor.com/m/Vx-Fmk3G7McAAAAC/cute.gif" />
           <div className="my-4 text-4xl font-bold">
             WOOOOOO!!! I love you pookie!! ;))
           </div>
@@ -48,7 +46,9 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Can I be your boyfriend?</h1>
+          <h1 className="my-4 text-4xl">
+            Can I be your Bestesttt Friend Again?
+          </h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
